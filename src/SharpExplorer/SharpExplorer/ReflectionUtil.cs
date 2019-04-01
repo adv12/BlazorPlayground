f7c247df50abf1042374db3755fe937b9cbfb7ee
Andrew Vardeman <andrewdev@the-vardemans.net>
2019-03-31 21:03:14 -0500

    Got a basic page layout working; refactored code from the index page into a FilteredListView razor component

---
+++ src/SharpExplorer/SharpExplorer/ReflectionUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SharpExplorer
+{
+    public class ReflectionUtil
+    {
+        private static SortedList<string, Type> _allTypes;
+        public static SortedList<string, Type> AllTypes
+        {
+            get
+            {
+                if (_allTypes == null)
+                {
+                    _allTypes = new SortedList<string, Type>();
+                    foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        foreach (Type type in assembly.GetTypes())
+                        {
+                            if (type.IsPublic)
+                            {
+                                _allTypes[type.FullName] = type;
+                            }
+                        }
+                    }
+                }
+                return _allTypes;
+            }
+        }
+
+    }
+}
---
+++ src/SharpExplorer/SharpExplorer/Row.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpExplorer
+{
+    public class Row
+    {
+        public object Value { get; set; }
+        public string FilterString { get; set; }
+        public string[] Cells { get; set; }
+        public Row(object value, string filterString, params string[] cells)
+        {
+            Value = value;
+            FilterString = filterString?.ToLower();
+            Cells = cells;
+        }
+    }
+}
