083d94c4f2fd05ae6f910872ce93c128da12fae3
Andrew Vardeman <andrewdev@the-vardemans.net>
2019-04-07 22:50:32 -0500

    Learned that I can turn linking off and send full .NET dlls to Blazor, which makes this project interesting again.  Implemented some more... stuff.

--- src/SharpExplorer/SharpExplorer/ReflectionUtil.cs
+++ src/SharpExplorer/SharpExplorer/ReflectionUtil.cs
@@ -6,26 +6,107 @@ namespace SharpExplorer
 {
     public class ReflectionUtil
     {
-        private static SortedList<string, Type> _allTypes;
-        public static SortedList<string, Type> AllTypes
+        private static SortedList<string, Type> _allPublicTypes;
+        private static SortedList<string, Type> _allPublicClasses;
+        private static SortedList<string, Type> _allPublicClassesWithPublicConstructors;
+        private static SortedList<string, Type> _allPublicClassesWithPublicStaticMethods;
+        private static SortedList<string, Type> _allPublicClassesWithPublicStaticProperties;
+
+        public static SortedList<string, Type> AllPublicTypes
         {
             get
             {
-                if (_allTypes == null)
+                if (_allPublicTypes == null)
                 {
-                    _allTypes = new SortedList<string, Type>();
+                    _allPublicTypes = new SortedList<string, Type>();
                     foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
                     {
                         foreach (Type type in assembly.GetTypes())
                         {
                             if (type.IsPublic)
                             {
-                                _allTypes[type.FullName] = type;
+                                _allPublicTypes[type.FullName] = type;
                             }
                         }
                     }
                 }
-                return _allTypes;
+                return _allPublicTypes;
+            }
+        }
+
+        public static SortedList<string, Type> AllPublicClasses
+        {
+            get
+            {
+                if (_allPublicClasses == null)
+                {
+                    _allPublicClasses = new SortedList<string, Type>();
+                    foreach (Type type in AllPublicTypes.Values)
+                    {
+                        if (type.IsClass)
+                        {
+                            _allPublicClasses[type.FullName] = type;
+                        }
+                    }
+                }
+                return _allPublicClasses;
+            }
+        }
+
+        public static SortedList<string, Type> AllPublicClassesWithPublicConstructors
+        {
+            get
+            {
+                if (_allPublicClassesWithPublicConstructors == null)
+                {
+                    _allPublicClassesWithPublicConstructors = new SortedList<string, Type>();
+                    foreach (Type type in AllPublicClasses.Values)
+                    {
+                        if (type.GetConstructors().Length > 0)
+                        {
+                            _allPublicClassesWithPublicConstructors[type.FullName] = type;
+                        }
+                    }
+                }
+                return _allPublicClassesWithPublicConstructors;
+            }
+        }
+
+        public static SortedList<string, Type> AllPublicClassesWithPublicStaticMethods
+        {
+            get
+            {
+                if (_allPublicClassesWithPublicStaticMethods == null)
+                {
+                    _allPublicClassesWithPublicStaticMethods = new SortedList<string, Type>();
+                    foreach (Type type in AllPublicClasses.Values)
+                    {
+                        if (type.GetMethods(BindingFlags.Public | BindingFlags.Static).Length > 0)
+                        {
+                            _allPublicClassesWithPublicStaticMethods[type.FullName] = type;
+                        }
+                    }
+                }
+                return _allPublicClassesWithPublicStaticMethods;
+            }
+        }
+
+        public static SortedList<string, Type> AllPublicClassesWithPublicStaticProperties
+        {
+            get
+            {
+                if (_allPublicClassesWithPublicStaticProperties == null)
+                {
+                    _allPublicClassesWithPublicStaticProperties = new SortedList<string, Type>();
+                    foreach (Type type in AllPublicClasses.Values)
+                    {
+                        if (type.GetProperties(BindingFlags.Public | BindingFlags.Static).Length > 0)
+                        {
+                            _allPublicClassesWithPublicStaticProperties[type.FullName] = type;
+                        }
+                    }
+                }
+                return _allPublicClassesWithPublicStaticProperties;
             }
         }
 
